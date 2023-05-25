390422d8e45da1f265d99c062a780dd9eb72f905
tgartland89 <thomas.gartland@gmail.com>
2023-05-25 08:51:09 -0600

    reset back to lab state

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Nav from "./Nav";
-import Pigs from "./Pigs"
 
 import hogs from "../porkers_data";
 
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Pigs hogs={hogs}></Pigs>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Nav />
+		</div>
+	);
 }
 
 export default App;
